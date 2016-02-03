7c07f79a8ff4d5131401f20bd40df2164847afb2
Sauryanjan Bhoi <sauryanjan.bhoi1@genpact.com>
2016-02-03 17:00:16 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myDemoApp
+ */
+angular.module('myDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
